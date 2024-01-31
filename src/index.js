0535e97897018d0dc2f51dccdec3fa71a77e4ac1
Shawon <shawonorib@gmail.com>
2024-01-31 13:09:26 +0600

    util code discussion

---
+++ src/index.js
@@ -0,0 +1,73 @@
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+
+
+
+
+
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERROR: ", error);
+        throw error
+    })
+
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catah((err)=>{
+    console.log("MONGO db connection failed!!!", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+//1ST APPROACH TO CONNECT DB
+import express from "express"
+const app = express();
+
+;(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERROR ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})() */
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,33 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler).catch((err)=>next(err))
+    }
+}
+
+
+
+
+
+export {asyncHandler}
+
+/* 
+//Higher Order function লেখার নিয়ম step by step:
+const asyncHandler = () => {}
+const asyncHandler = (function) => {()=>{}}
+const asyncHandler = (function) => ()=>{}
+const asyncHandler = (function) => async ()=>{} 
+
+
+//try-catch দিয়ে কিভাবে request handle করবোঃ
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}   
+
+*/
